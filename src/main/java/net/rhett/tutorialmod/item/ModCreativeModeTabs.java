d388c69089a048388ea166663ed7d9aa3c10ad46
Rhett <rhettpalmer1@gmail.com>
2024-01-17 00:00:44 -0700

    Added new directories for blocks as well as two new blocks

--- src/main/java/net/rhett/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/rhett/tutorialmod/item/ModCreativeModeTabs.java
@@ -8,6 +8,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import net.rhett.tutorialmod.TutorialMod;
+import net.rhett.tutorialmod.block.ModBlocks;
 
 public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
@@ -18,8 +19,14 @@ public class ModCreativeModeTabs {
             () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.SAPPHIRE.get()))
                     .title(Component.translatable("creativetab.tutorial_tab"))
                     .displayItems((pParamaters, pOutput) -> {
+
+
                         pOutput.accept(ModItems.SAPPHIRE.get());
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
+
+
+                        pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
 
 
                     })
